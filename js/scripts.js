816534468190993c0e1985d0aee8441ebf3c385a
ramza007 <ramzalvo007@gmail.com>
2018-02-16 20:01:37 +0300

    added scripts.js and styles.css

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("Nav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("Nav").style.width = "0%";
+}
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+ gtag('config', 'GA_TRACKING_ID');
